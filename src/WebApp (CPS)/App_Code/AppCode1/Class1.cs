daeb400a9f5608428751c6537b509cd39dedfae0
David Kean <davkean@microsoft.com>
2020-11-03 10:39:06 +1100

    AppCode

---
+++ src/WebApp (CPS)/App_Code/AppCode1/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebApp__CPS_.App_Code.AppCode1
+{
+    class Class1
+    {
+    }
+}
